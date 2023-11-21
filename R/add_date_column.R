c5680fc9f53f65bac124dd4efbcdb0a39df79ed7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-21 15:01:29 -0800

    ✅ Pass tests

--- R/add_date_column.R
+++ R/add_date_column.R
@@ -1,5 +1,9 @@
 extract_date_from_filename <- function(file_name) {
-  "19/Nov/2023"
+  date <- stringr::str_sub(file_name, -13)
+  day <- stringr::str_sub(date, 1, 2)
+  month <- stringr::str_to_title(stringr::str_sub(date, 3, 5))
+  year <- stringr::str_sub(date, 6, 9)
+  glue::glue("{day}/{month}/{year}")
 }
 
 return_one <- function() {
