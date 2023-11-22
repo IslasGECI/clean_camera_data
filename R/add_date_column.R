aa0537fd497276ad579ac636b76da88a33aab48b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-22 14:51:09 -0800

    ✅ Pass tests

--- R/add_date_column.R
+++ R/add_date_column.R
@@ -10,3 +10,8 @@ add_column_fecha_envio <- function(raw_data, date_send_data) {
   raw_data |>
     dplyr::mutate(Fecha_envio_datos = date_send_data)
 }
+
+add_column_fecha_envio_revision_campo <- function(raw_data, date_send_data) {
+  raw_data |>
+    dplyr::mutate(Fecha_envio_datos = date_send_data, .after = "Fecha_revision")
+}
