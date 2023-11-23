5bf6d017a7ac7bd4e68741ba15665432c72abf65
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-23 11:57:39 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -4,5 +4,9 @@ describe("Cli for module", {
     xlsx_name <- "IG_CAMARA_TRAMPA_EXTRA_23NOV2023.xls"
     add_data_check_column_extra_campo(xlsx_name, csv_name)
     expect_true(testtools::exist_output_file("/workdir/tests/data/cameras_extra_campo_with_date.csv"))
+    obtained <- readr::read_csv("/workdir/tests/data/cameras_extra_campo_with_date.csv")
+    obtained_ncol <- ncol(obtained)
+    expected_ncol <- 4
+    expect_equal(obtained_ncol, expected_ncol)
   })
 })
