b466710481b102edd9b569f626b8182d23f45d24
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-11-23 11:46:34 -0800

    🗃️✔️Add test data

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -1,6 +1,6 @@
 describe("Cli for module", {
   it("add_column", {
-    csv_name <- "cameras_extra_revision_campo.csv"
+    csv_name <- "/workdir/tests/data/cameras_extra_revision_campo.csv"
     xlsx_name <- "IG_CAMARA_TRAMPA_EXTRA_23NOV2023.xls"
     add_data_check_column_extra_campo(xlsx_name, csv_name)
     expect_true(testtools::exist_output_file(csv_name))
