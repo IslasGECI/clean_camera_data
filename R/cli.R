f77efef93ba8dfeb46ccfde41b7828c3122360b1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-23 11:53:56 -0800

    ♻️  Refactor

--- R/cli.R
+++ R/cli.R
@@ -1,4 +1,4 @@
 add_data_check_column_extra_campo <- function(xlsx_name, csv_name) {
-  readr::read_csv(csv_name) |>
+  readr::read_csv(csv_name, show_col_types = FALSE) |>
     readr::write_csv("/workdir/tests/data/cameras_extra_campo_with_date.csv")
 }
