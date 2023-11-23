86a69f58ffa61c199a92774c79321727e4fd0e0f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-23 12:04:04 -0800

    ♻️  Refactor

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -5,7 +5,7 @@ describe("Cli for module", {
     add_data_check_column_extra_campo(xlsx_name, csv_name)
     output_with_date <- "/workdir/tests/data/cameras_extra_campo_with_date.csv"
     expect_true(testtools::exist_output_file(output_with_date))
-    obtained <- readr::read_csv(output_with_date)
+    obtained <- readr::read_csv(output_with_date, show_col_types = FALSE)
     obtained_ncol <- ncol(obtained)
     expected_ncol <- 4
     expect_equal(obtained_ncol, expected_ncol)
