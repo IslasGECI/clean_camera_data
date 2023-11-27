b6c29527c4bc48be68c94db1249ba73d91a5a8f4
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-27 09:37:06 -0800

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -1,4 +1,4 @@
-add_data_check_column_extra_campo <- function(xlsx_name, csv_name) {
+add_data_check_column_extra_campo <- function(xlsx_name, csv_name, output_path) {
   raw_data <- readr::read_csv(csv_name, show_col_types = FALSE)
   date_send_data <- extract_date_from_filename(xlsx_name)
   add_column_fecha_envio_revision_campo(raw_data, date_send_data) |>
