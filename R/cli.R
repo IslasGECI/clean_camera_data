1d37ea70e4d886d3e9c1e17248c0e6c4db4eb323
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-27 09:40:55 -0800

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -1,6 +1,6 @@
-add_data_check_column_extra_campo <- function(xlsx_name, csv_name, output_path) {
+add_data_check_column_extra_campo <- function(xlsx_name, csv_name, output_path = "/workdir/cameras_extra_revision_campo.csv") {
   raw_data <- readr::read_csv(csv_name, show_col_types = FALSE)
   date_send_data <- extract_date_from_filename(xlsx_name)
   add_column_fecha_envio_revision_campo(raw_data, date_send_data) |>
-    readr::write_csv("/workdir/tests/data/cameras_extra_campo_with_date.csv")
+    readr::write_csv(output_path)
 }
