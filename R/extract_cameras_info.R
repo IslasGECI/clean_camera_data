d60bddc87e2435c0204574f152f86866066fba9a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-29 11:29:05 -0800

    ♻️  Refactor

--- R/extract_cameras_info.R
+++ R/extract_cameras_info.R
@@ -1,4 +1,4 @@
 extract_cameras_info <- function(revision_campo_tibble) {
   columns_of_interest <- c("ID_camara", "Coordenada_Este", "Coordenada_Norte", "Ultima_revision", "Lineas")
-  return(revision_campo_tibble |> dplyr::select(columns_of_interest))
+  return(revision_campo_tibble |> dplyr::select(all_of(columns_of_interest)))
 }
