a16b577164d17d4a8522806023b5a52d4ccceee5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-29 11:45:00 -0800

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -13,3 +13,10 @@ add_data_check_column_to_memoria <- function(xlsx_name, csv_name, output_path = 
   add_column_fecha_envio_revision_memoria(raw_data, date_send_data) |>
     readr::write_csv(output_path)
 }
+
+#' @export
+write_camera_info <- function(csv_name, output_path) {
+  raw_data <- readr::read_csv(csv_name, show_col_types = FALSE)
+  extract_cameras_info(raw_data) |>
+    readr::write_csv(output_path)
+}
