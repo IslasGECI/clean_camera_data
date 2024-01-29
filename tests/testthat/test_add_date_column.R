0893567423dc7d8ab66407d98664dd57d2cc61b3
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-29 11:06:59 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_add_date_column.R
+++ tests/testthat/test_add_date_column.R
@@ -34,7 +34,7 @@ describe("Add column 'Fecha_envio_datos' after columns ''", {
     expect_equal(obtained, expected)
   })
   it("Add column to Revision Memoria", {
-    raw_data <- tibble::tibble("Fotos capturadas" = 1:3, "Fecha_captura_foto" = letters[1:3])
+    raw_data <- tibble::tibble("Fotos capturadas" = 1:3, "Fecha_captura_foto" = letters[1:3], "Lineas" = 4:6)
     expected <- tibble::tibble("Fotos capturadas" = 1:3, Fecha_envio_datos = rep("25/Dic/2023", 3), "Fecha_captura_foto" = letters[1:3])
     obtained <- add_column_fecha_envio_revision_memoria(raw_data, "25/Dic/2023")
     expect_equal(obtained, expected)
