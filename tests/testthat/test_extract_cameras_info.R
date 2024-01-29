ce2fe98081745d618e17fff0af758defd5b306a8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-29 11:26:10 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_extract_cameras_info.R
+++ tests/testthat/test_extract_cameras_info.R
@@ -2,5 +2,8 @@ describe("extract cameras info", {
   it("extract_cameras_info", {
     revision_campo_tibble <- readr::read_csv("/workdir/tests/data/cameras_extra_revision_campo_with_coordinates.csv", show_col_types = FALSE)
     obtained <- extract_cameras_info(revision_campo_tibble)
+    obtained_ncol <- ncol(obtained)
+    expected_ncol <- 5
+    expect_equal(obtained_ncol, expected_ncol)
   })
 })
