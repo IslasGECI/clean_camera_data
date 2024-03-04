2acb32c7dfabecc86e1bada88b8fa876d61a2a36
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-04 12:57:11 -0800

    ✅ Pass tests

--- R/extract_cameras_info.R
+++ R/extract_cameras_info.R
@@ -9,6 +9,6 @@ select_last_check_date <- function(cameras_with_check_date) {
 
 
 extract_cameras_info <- function(revision_campo_tibble) {
-  columns_of_interest <- c("ID_camara", "Coordenada_Este", "Coordenada_Norte", "Ultima_revision", "Lineas")
+  columns_of_interest <- c("ID_camara", "Coordenada_Este", "Coordenada_Norte", "Lineas")
   return(revision_campo_tibble |> dplyr::select(all_of(columns_of_interest)))
 }
