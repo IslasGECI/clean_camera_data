afe838060294f0e9f16a5b0667530e288528ea49
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-08-19 10:40:26 -0700

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -11,6 +11,7 @@ add_data_check_column_to_memoria <- function(xlsx_name, csv_name, output_path = 
   raw_data <- readr::read_csv(csv_name, show_col_types = FALSE)
   date_send_data <- extract_date_from_filename(xlsx_name)
   add_column_fecha_envio_revision_memoria(raw_data, date_send_data) |>
+    dplyr::distinct() |>
     readr::write_csv(output_path)
 }
 
