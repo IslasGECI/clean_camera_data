1b51277f49d876f011fbc8e4cc5f4dda748c8c89
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-12-05 14:54:47 -0800

    ♻️  Refactor

--- R/calculate_effort.R
+++ R/calculate_effort.R
@@ -15,13 +15,8 @@ get_ocassion <- function(raw_data_with_date) {
   return(trapping_hunting_with_ocassions)
 }
 get_week_of_year_from_date <- function(date) {
-  year <- lubridate::year(date)
-  first_day_of_year_string <- paste0(year, "-01-01")
-  first_week_of_year <- lubridate::isoweek(first_day_of_year_string)
   week_of_year <- lubridate::isoweek(date)
-  if (first_week_of_year >= 52) {
-    week_of_year <- week_of_year + 1
-  }
+  week_of_year <- is_first_day_of_year_in_first_week(date, week_of_year)
   month_of_year <- lubridate::month(date)
   if ((month_of_year == 1) & (week_of_year >= 52)) {
     week_of_year <- 1
@@ -32,6 +27,15 @@ get_week_of_year_from_date <- function(date) {
     } else if (week_of_year == 2) {
       week_of_year <- 54
     }
+  }
+  return(week_of_year)
+}
+is_first_day_of_year_in_first_week <- function(date, week_of_year) {
+  year <- lubridate::year(date)
+  first_day_of_year_string <- paste0(year, "-01-01")
+  first_week_of_year <- lubridate::isoweek(first_day_of_year_string)
+  if (first_week_of_year >= 52) {
+    week_of_year <- week_of_year + 1
   }
   return(week_of_year)
 }
