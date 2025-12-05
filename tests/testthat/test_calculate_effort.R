68255ee3818ef31465f34c15031934b0c9dab8ae
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-12-05 13:11:48 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_calculate_effort.R
+++ tests/testthat/test_calculate_effort.R
@@ -2,7 +2,8 @@ describe("Calculate effor by month and week", {
   revision_campo <- readr::read_csv("/workdir/tests/data/revision_campo_cameras_example.csv", show_col_types = FALSE)
   it("check columns", {
     obtained_effort <- calculate_effort(revision_campo)
+    obtained_columns <- colnames(obtained_effort)
     expected_columns <- c("e", "Session", "Ocassion")
-    expect_true(colnames(obtained_effort) %in% expected_columns)
+    expect_true(all(obtained_columns %in% expected_columns))
   })
 })
