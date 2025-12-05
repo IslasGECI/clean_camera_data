9108f4cb9759898dc655975755d36b354f893227
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-12-05 13:29:41 -0800

    ✅ Pass tests

--- R/calculate_effort.R
+++ R/calculate_effort.R
@@ -1,3 +1,10 @@
 calculate_effort <- function(revision_campo) {
   tibble::tibble(e = 1, Session = 2, Ocassion = 3)
 }
+get_session <- function(data_trapping_hunting) {
+  months <- lubridate::month(data_trapping_hunting$Date)
+  years <- lubridate::year(data_trapping_hunting$Date)
+  sessions <- data_trapping_hunting %>%
+    dplyr::mutate(Session = paste(years, months, sep = "-"))
+  return(sessions)
+}
