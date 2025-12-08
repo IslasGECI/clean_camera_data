33d4d4bf27795c3c6f377d53f5f8924e96ec63bb
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-12-08 14:50:44 -0800

    ✅ Pass tests

--- R/calculate_effort.R
+++ R/calculate_effort.R
@@ -12,7 +12,7 @@ fill_ocassions <- function(effort_data) {
     tidyr::complete(Date = seq.Date(min(Date, na.rm = TRUE), max(Date, na.rm = TRUE), by = "1 week")) |>
     dplyr::ungroup() |>
     get_ocassion() |>
-    tidyr::fill(c(ID_camara_trampa, Revision), .direction = "down")
+    tidyr::fill(c(ID_camara_trampa, Revision, Estado_camara), .direction = "down")
   return(effort_data_filled)
 }
 get_session <- function(raw_data_with_date) {
