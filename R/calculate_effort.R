b8478becfb05adf9fb65d16344018a4a44bd6cba
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-12-08 13:27:59 -0800

    ✅ Pass tests

--- R/calculate_effort.R
+++ R/calculate_effort.R
@@ -3,7 +3,7 @@ change_to_tidy_effort_format <- function(revision_campo) {
     dplyr::rename(Date = Fecha_envio_datos) |>
     get_ocassion() |>
     tidyr::complete(Ocassion = tidyr::full_seq(Ocassion, 1)) |>
-    dplyr::mutate(e = 7, Session = 2)
+    dplyr::mutate(e = dplyr::case_when(Estado_camara == "D" ~ 0, TRUE ~ 7), Session = 2)
 }
 get_session <- function(raw_data_with_date) {
   months <- lubridate::month(raw_data_with_date$Date)
