ae9905a1c0f9620a3c0b54b0d87feadb7f10cccf
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-01-15 11:30:07 -0800

    ✅ Pass tests

--- R/join_cameras_info.R
+++ R/join_cameras_info.R
@@ -1,5 +1,6 @@
 join_cameras_info <- function(revision_campo_df, revision_memoria_df) {
-  dplyr::full_join(revision_campo_df, revision_memoria_df, by = dplyr::join_by(ID_camara_trampa == ID_camara, Fecha_envio_datos)) |>
+  revision_memoria_summary <- get_weekly_pictures_summary(revision_memoria_df)
+  dplyr::full_join(revision_campo_df, revision_memoria_summary, by = dplyr::join_by(ID_camara_trampa == ID_camara, Fecha_envio_datos)) |>
     dplyr::select(c("ID_camara_trampa", "Fotos_capturadas", "Fecha_envio_datos", "Fecha_revision_campo", "Individuos_capturados", "Estado_camara"))
 }
 
