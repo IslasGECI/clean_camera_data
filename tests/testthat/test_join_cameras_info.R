2a85078a98dac0f3630f31b9c358e44a48ab1be5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-01-15 10:48:17 -0800

    🛑🧪 Fail tests

---
+++ tests/testthat/test_join_cameras_info.R
@@ -0,0 +1,9 @@
+describe("join into one table the info from camera traps", {
+  revision_campo_df <- readr::read_csv("tests/data/camaras_revision_campo.csv", show_col_types = FALSE)
+  revision_memoria_df <- readr::read_csv("tests/data/camaras_revision_memoria.csv", show_col_types = FALSE)
+  it("assert columns of joined", {
+    obtained <- join_cameras_info(revision_campo_df, revision_memoria_df)
+    expected_columns <- c("ID_camara_trampa", "Fotos_capturadas", "Fecha_envio_datos", "Fecha_revision_campo", "Individuos_capturados", "Estado_camara")
+    expect_true(all(expected_columns %in% colnames(obtained)))
+  })
+})
