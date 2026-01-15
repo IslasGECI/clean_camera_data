ecba8c1dd55ec06d4055d157d5cb06ff518faebb
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-01-15 10:53:21 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_join_cameras_info.R
+++ tests/testthat/test_join_cameras_info.R
@@ -4,6 +4,8 @@ describe("join into one table the info from camera traps", {
   it("assert columns of joined", {
     obtained <- join_cameras_info(revision_campo_df, revision_memoria_df)
     expected_columns <- c("ID_camara_trampa", "Fotos_capturadas", "Fecha_envio_datos", "Fecha_revision_campo", "Individuos_capturados", "Estado_camara")
-    expect_true(all(expected_columns %in% colnames(obtained)))
+    obtained_colnames <- colnames(obtained)
+    expect_true(all(expected_columns %in% obtained_colnames))
+    expect_equal(length(expected_columns), length(obtained_colnames))
   })
 })
