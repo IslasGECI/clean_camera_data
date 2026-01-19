0c9491df319c1f9da792b9efa88dc1c8c78ac651
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-01-19 12:48:51 -0800

    ♻️ Refactor

--- R/join_cameras_info.R
+++ R/join_cameras_info.R
@@ -3,14 +3,17 @@ calculate_cameras_summary <- function(revision_campo_df, revision_memoria_df) {
     dplyr::filter(Revision == "si") |>
     join_cameras_info(revision_memoria_df)
   summary_df <- get_cameras_effort(joined_cameras_info) |>
-    dplyr::group_by(Fecha_envio_datos) |>
-    dplyr::summarise(Number_of_camera_traps = dplyr::n_distinct(ID_camara_trampa), Effort = sum(effort), Total_photos = sum(Fotos_capturadas), Total_individuals = sum(Individuos_capturados)) |>
-    dplyr::ungroup()
+    summarise_cameras_info()
   summary_df |>
     dplyr::mutate(Date = Fecha_envio_datos) |>
     dplyr::select(c("Date", "Number_of_camera_traps", "Effort", "Total_photos", "Total_individuals"))
 }
-
+summarise_cameras_info <- function(joined_cameras_with_effort) {
+  joined_cameras_with_effort |>
+    dplyr::group_by(Fecha_envio_datos) |>
+    dplyr::summarise(Number_of_camera_traps = dplyr::n_distinct(ID_camara_trampa), Effort = sum(effort), Total_photos = sum(Fotos_capturadas), Total_individuals = sum(Individuos_capturados)) |>
+    dplyr::ungroup()
+}
 join_cameras_info <- function(revision_campo_df, revision_memoria_df) {
   revision_memoria_summary <- get_weekly_pictures_summary(revision_memoria_df)
   dplyr::full_join(revision_campo_df, revision_memoria_summary, by = dplyr::join_by(ID_camara_trampa == ID_camara, Fecha_envio_datos)) |>
