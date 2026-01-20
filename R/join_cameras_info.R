a875c49a44ff81c6997ac7d679dd935a88c34fe2
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-01-20 10:53:08 -0800

    ✅ Pass tests

--- R/join_cameras_info.R
+++ R/join_cameras_info.R
@@ -4,28 +4,12 @@ calculate_cameras_summary <- function(revision_campo_df, revision_memoria_df) {
     summarise_cameras_info() |>
     dplyr::rename(Date = Fecha_envio_datos)
 }
-summarise_cameras_info <- function(joined_cameras_with_effort) {
-  joined_cameras_with_effort |>
-    dplyr::group_by(Fecha_envio_datos) |>
-    dplyr::summarise(Number_of_camera_traps = dplyr::n_distinct(ID_camara_trampa), Effort = sum(effort), Total_photos = sum(Fotos_capturadas), Total_individuals = sum(Individuos_capturados)) |>
-    dplyr::ungroup()
-}
 join_cameras_info <- function(revision_campo_df, revision_memoria_df) {
   revision_campo_filtered <- dplyr::filter(revision_campo_df, Revision == "si")
   revision_memoria_summary <- get_weekly_pictures_summary(revision_memoria_df)
   dplyr::full_join(revision_campo_filtered, revision_memoria_summary, by = dplyr::join_by(ID_camara_trampa == ID_camara, Fecha_envio_datos)) |>
     dplyr::select(c("ID_camara_trampa", "Fotos_capturadas", "Fecha_envio_datos", "Fecha_revision_campo", "Individuos_capturados", "Estado_camara"))
 }
-
-get_weekly_pictures_summary <- function(revision_memoria_df) {
-  revision_memoria_df |>
-    dplyr::group_by(ID_camara, Fecha_envio_datos) |>
-    dplyr::summarise(
-      Fotos_capturadas = dplyr::first(Fotos_capturadas),
-      Individuos_capturados = sum(Individuos_capturados, na.rm = TRUE)
-    )
-}
-
 get_cameras_effort <- function(joined_cameras_info) {
   joined_cameras_info |>
     transform_spanish_dates_to_iso_format() |>
@@ -37,6 +21,27 @@ get_cameras_effort <- function(joined_cameras_info) {
       TRUE ~ 0
     ))
 }
+summarise_cameras_info <- function(joined_cameras_with_effort) {
+  joined_cameras_with_effort |>
+    dplyr::group_by(Fecha_envio_datos) |>
+    dplyr::summarise(
+      Number_of_camera_traps = dplyr::n_distinct(ID_camara_trampa),
+      Effort = sum(effort, na.rm = TRUE),
+      Total_photos = sum(Fotos_capturadas, na.rm = TRUE),
+      Total_individuals = sum(Individuos_capturados, na.rm = TRUE)
+    ) |>
+    dplyr::ungroup()
+}
+
+get_weekly_pictures_summary <- function(revision_memoria_df) {
+  revision_memoria_df |>
+    dplyr::group_by(ID_camara, Fecha_envio_datos) |>
+    dplyr::summarise(
+      Fotos_capturadas = dplyr::first(Fotos_capturadas),
+      Individuos_capturados = sum(Individuos_capturados, na.rm = TRUE)
+    )
+}
+
 transform_spanish_dates_to_iso_format <- function(joined_cameras_info) {
   joined_cameras_info |>
     dplyr::mutate(Fecha = Fecha_envio_datos) |>
