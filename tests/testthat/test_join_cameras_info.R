eb51a3d9c6da460c0fb2603a4b9f17e3d37e5fd0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-05 13:12:42 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_join_cameras_info.R
+++ tests/testthat/test_join_cameras_info.R
@@ -20,7 +20,7 @@ describe("calculate cameras summary", {
     expected_total_individuals <- 2
     expect_equal(obtained$Total_individuals[october_nineteen_row], expected_total_individuals)
 
-    october_fourth_row <- 3
+    october_fourth_row <- 1
     expected_total_photos <- 351
     expect_equal(obtained$Total_photos[october_fourth_row], expected_total_photos)
   })
