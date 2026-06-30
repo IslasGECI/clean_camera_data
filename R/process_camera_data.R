d7b4139cb942b1ca970ac40f1ea70949e6942849
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-06-30 17:42:56 +0000

    ✅ Pass tests

--- R/process_camera_data.R
+++ R/process_camera_data.R
@@ -1,1 +1,3 @@
-compute_daily_status <- function(cameras_campo_df) {}
+compute_daily_status <- function(cameras_campo_df) {
+  cameras_campo_df |> dplyr::rename(Date = Fecha_revision_campo, ID = ID_camara_trampa, camera_status = Estado_camara)
+}
