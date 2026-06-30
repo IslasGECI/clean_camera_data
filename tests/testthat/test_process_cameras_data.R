0525783a813ff4c1088a538144737603d8e5e253
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-06-30 17:45:50 +0000

    🛑🧪 Fail tests

--- tests/testthat/test_process_cameras_data.R
+++ tests/testthat/test_process_cameras_data.R
@@ -1,6 +1,6 @@
 describe("Create cameras daily status", {
   cameras_campo_df <- tibble::tibble(
-    "Fecha_revision_campo" = c(rep("2022-01-02", 4), rep("2022-01-03", 3)),
+    "Fecha_revision_campo" = c(rep("2022-01-02", 4), rep("2022-01-04", 3)),
     "ID_camara_trampa" = c("CT-01-xxx-CT", "CT-01-yyy-CT", "CT-01-zzz-CT", "CT-01-www-CT", "CT-01-xxx-CT", "CT-01-yyy-CT", "CT-01-zzz-CT"),
     "Revision" = rep("si", 7),
     "Estado_camara" = rep("A", 7)
@@ -10,5 +10,7 @@ describe("Create cameras daily status", {
     expected_column_names <- c("Date", "ID", "camera_status")
     obtained_column_names <- colnames(obtained)
     expect_true(all(expected_column_names %in% obtained_column_names))
+    expected_rows <- 10
+    expect_equal(nrow(obtained), expected_rows)
   })
 })
