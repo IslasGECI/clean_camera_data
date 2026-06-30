68db11e4d1977449b7f04ee7e934c49278ea2d74
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-06-30 18:08:45 +0000

    🛑🧪 Fail tests

--- tests/testthat/test_process_cameras_data.R
+++ tests/testthat/test_process_cameras_data.R
@@ -3,11 +3,18 @@ describe("Create cameras daily status", {
     "Fecha_revision_campo" = c(rep("2022-01-02", 4), rep("2022-01-04", 3)),
     "ID_camara_trampa" = c("CT-01-xxx-CT", "CT-01-yyy-CT", "CT-01-zzz-CT", "CT-01-www-CT", "CT-01-xxx-CT", "CT-01-yyy-CT", "CT-01-zzz-CT"),
     "Revision" = rep("si", 7),
-    "Estado_camara" = rep("A", 7)
+    "Estado_camara" = c("R", rep("A", 6))
+  )
+  cameras_memoria_df <- tibble::tibble(
+    "Fecha_captura_foto" = c(rep(NA, 4), NA, "2022-01-03", "2022-01-04"),
+    "ID_camara" = c("CT-01-xxx-CT", "CT-01-yyy-CT", "CT-01-zzz-CT", "CT-01-www-CT", "CT-01-xxx-CT", "CT-01-yyy-CT", "CT-01-zzz-CT"),
+    "Individuos_capturados" = c(rep(NA, 2), rep(0, 3), 1, 2),
+    "Fotos_capturadas" = c(rep(NA, 5), 100, 1900)
   )
   it("compute_daily_status", {
-    obtained <- compute_daily_status(cameras_campo_df)
-    expected_column_names <- c("Date", "ID", "camera_status")
+    obtained <- compute_daily_status(cameras_campo_df, cameras_memoria_df)
+    print(obtained)
+    expected_column_names <- c("Date", "ID", "camera_status", "Individuos_capturados", "Fotos_capturadas")
     obtained_column_names <- colnames(obtained)
     expect_true(all(expected_column_names %in% obtained_column_names))
     expected_rows <- 10
