c2bc76cbaae8293db53c5ee61acccc7ee112acba
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-08-04 22:00:06 +0000

    🛑🧪 Fail tests

--- tests/testthat/test_process_cameras_data.R
+++ tests/testthat/test_process_cameras_data.R
@@ -18,7 +18,7 @@ describe("Create cameras daily status", {
     expected_column_names <- c("Date", "ID", "camera_status", "Individuos_capturados", "Fotos_capturadas")
     obtained_column_names <- colnames(obtained)
     expect_true(all(expected_column_names %in% obtained_column_names))
-    expected_rows <- 19
+    expected_rows <- 24
     expect_equal(nrow(obtained), expected_rows)
     expected_individuals_for_ct_zzz <- 2
     expect_equal(obtained[obtained$ID == "CT-01-zzz-CT" & obtained$Date == "2022-01-04", ]$Individuos_capturados, expected_individuals_for_ct_zzz)
@@ -26,5 +26,7 @@ describe("Create cameras daily status", {
     expect_equal(obtained[obtained$ID == "CT-01-www-CT", ]$camera_status, expected_status_for_ct_www)
     expected_status_for_ct_ad1 <- c(rep("A", 3), rep("D", 2))
     expect_equal(obtained[obtained$ID == "CT-01-ad1-CT", ]$camera_status, expected_status_for_ct_ad1)
+    expected_status_for_ct_ad2 <- c(rep("A", 3), rep("D", 3))
+    expect_equal(obtained[obtained$ID == "CT-01-ad2-CT", ]$camera_status, expected_status_for_ct_ad2)
   })
 })
